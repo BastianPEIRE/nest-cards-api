9076669db4f00fb272ccbcbd83cede610c6062a4
BastianPEIRE <bastian@triliz.com>
2022-06-28 18:04:32 +0200

    feat(interfaces):init interfaces

--- src/cards/cards.interface.ts
+++ src/cards/cards.interface.ts
@@ -1,8 +1,5 @@
 export interface Cards {
     id?: string;
-    firstName: string;
-    lastName: string;
-    companyName: string;
     textColor: string;
     color: string;
 }
--- src/profiles/profiles.interface.ts
+++ src/profiles/profiles.interface.ts
@@ -1,1 +1,11 @@
-export interface Profiles {}
+import { Cards } from "src/cards/cards.interface";
+
+export interface Profiles {
+    profileID?: string;
+    userId: string;
+    firstName: string;
+    lastName: string;
+    companyName: string;
+    email: string;
+    cards: Cards[];
+}
