dd3aee402f32d4ccd912e2439e5fd4b6e9f1f695
Bastian PEIRE <bastian.peire@gmail.com>
2022-06-29 00:28:53 +0200

    init jwt token and guards from doc

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { ProfilesController } from './profiles/profiles.controller';
 import { ProfilesService } from './profiles/profiles.service';
 import { ProfilesModule } from './profiles/profiles.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/27017'),
     CardsModule, 
-    ProfilesModule
+    ProfilesModule, AuthModule, UsersModule
   ],
   controllers: [AppController, ProfilesController],
   providers: [AppService, ProfilesService],
