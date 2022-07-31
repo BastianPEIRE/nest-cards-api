5415280c4bee22d4a66295b6c04c20d3056fc6d5
Bastian PEIRE <bastian.peire@gmail.com>
2022-07-31 13:27:48 +0200

    feat: creating ath controller module and service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
-import { UsersModule } from '../users/users.module';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './token/constants';
-import { JwtStrategy } from './token/jwt.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [
-    UsersModule,
-    PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
-    }),
-  ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
-  exports: [AuthService],
+  providers: [AuthService],
+  controllers: [AuthController]
 })
 export class AuthModule {}
