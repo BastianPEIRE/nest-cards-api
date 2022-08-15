ca6a5b6b71708c0277c7eacd94a176d137ebd81b
Bastian PEIRE <bastian.peire@gmail.com>
2022-08-15 15:53:38 +0200

    update from main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CardsModule } from './cards/cards.module';
-import { ProfilesController } from './profiles/profiles.controller';
-import { ProfilesService } from './profiles/profiles.service';
-import { ProfilesModule } from './profiles/profiles.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/27017'),
-    CardsModule, 
-    ProfilesModule, AuthModule, UsersModule
+    MongooseModule.forRoot(process.env.DB_URL, {}),
+    UserModule,
+    AuthModule,
   ],
-  controllers: [AppController, ProfilesController],
-  providers: [AppService, ProfilesService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
